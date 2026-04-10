SimenB/react-native-reanimated

commit 0deaeba14231c612aa9bc6f8c6caba7c13997131

chore: Limit `setViewStyle` calls in CSS to the bare minimum (#9104)
## Summary

After merging #8856, #8858 and other related PRs, CSS transitions no
longer access style properties in C++ so we can limit the `setViewStyle`
function calls to the CSS animations only. This PR implements this
simplification so that we no longer make unnecessary calls on every
render of every animated component, and instead, call this function now
only if the view has at least one CSS animation attached.

We likely could go further and detect which CSS animation actually needs
to read style properties in C++ but I think we can make such changes or
remove the necessity of storing styles in C++ after integration with the
animation backend is complete.

packages/react-native-reanimated/src/css/native/managers/CSSManager.ts
@@ -14,7 +14,6 @@ export default class CSSManager implements ICSSManager {
   private readonly cssTransitionsManager: CSSTransitionsManager;
   private readonly viewTag: number;
   private readonly propsBuilder: ReturnType<typeof getPropsBuilder>;
-  private isFirstUpdate: boolean = true;
 
   constructor(
     { shadowNodeWrapper, viewTag, reactViewName = 'RCTView' }: ViewInfo,
@@ -41,11 +40,15 @@ export default class CSSManager implements ICSSManager {
     const [animationProperties, transitionProperties, filteredStyle] =
       filterCSSAndStyleProperties(style);
 
-    const normalizedStyle = this.propsBuilder.build(filteredStyle);
+    const hasAnimationOrTransition =
+      animationProperties !== null || transitionProperties !== null;
+    const normalizedStyle = hasAnimationOrTransition
+      ? this.propsBuilder.build(filteredStyle)
+      : undefined;
 
-    // If the update is called during the first css style update, we won't
-    // trigger CSS transitions and set styles before attaching CSS transitions
-    if (this.isFirstUpdate && normalizedStyle) {
+    // We need to update view style only for animations (e.g. when a property is
+    // specified only in a subset of keyframes and start/end value comes from style).
+    if (normalizedStyle && animationProperties) {
       setViewStyle(this.viewTag, normalizedStyle);
     }
 
@@ -54,15 +57,6 @@ export default class CSSManager implements ICSSManager {
       normalizedStyle ?? {}
     );
     this.cssAnimationsManager.update(animationProperties);
-
-    // If the current update is not the fist one, we want to update CSS
-    // animations and transitions first and update the style then to make
-    // sure that the new transition is fired with new settings (like duration)
-    if (!this.isFirstUpdate && normalizedStyle) {
-      setViewStyle(this.viewTag, normalizedStyle);
-    }
-
-    this.isFirstUpdate = false;
   }
 
   unmountCleanup(): void {

commit 8ff895fdb039a0bf4c22b0ffa7a4bd591f44fbb6

fix: Incorrect CSS props reverting on CSS transition detach (#9220)
## Summary

The problem was caused by the android-specific props reverting logic in
CSS transitions. When transition is detached, we read current style
values from `StaticPropsRegistry` and commit them to the `ShadowTree`.

This issue was introduced in #9104 where I restricted `setViewStyle`
calls to CSS animations only as I though we no longer need to update the
style in css transitions.

## Example recordings

| Before | After |
|-|-|
| <video
src="https://github.com/user-attachments/assets/6cf2bc03-75eb-4caf-b9a2-471d350ff690"
/> | <video
src="https://github.com/user-attachments/assets/5c2900a7-e1f4-4198-8b7b-0f39488bffa4"
/> |

---------

Co-authored-by: Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>

packages/react-native-reanimated/src/css/native/managers/CSSManager.ts
@@ -1,5 +1,9 @@
 'use strict';
-import { getCompoundComponentName, getPropsBuilder } from '../../../common';
+import {
+  getCompoundComponentName,
+  getPropsBuilder,
+  IS_ANDROID,
+} from '../../../common';
 import type { ShadowNodeWrapper } from '../../../commonTypes';
 import type { ViewInfo } from '../../../createAnimatedComponent/commonTypes';
 import type { CSSStyle } from '../../types';
@@ -14,6 +18,13 @@ export default class CSSManager implements ICSSManager {
   private readonly cssTransitionsManager: CSSTransitionsManager;
   private readonly viewTag: number;
   private readonly propsBuilder: ReturnType<typeof getPropsBuilder>;
+  /**
+   * True if the previous update had CSS transition props attached. On the next
+   * update we still need to build `normalizedStyle` only on Android to revert
+   * props applied during the transition to correct current values. (fixes
+   * https://github.com/software-mansion/react-native-reanimated/issues/9218).
+   */
+  private hadTransitionLastUpdate = false;
 
   constructor(
     { shadowNodeWrapper, viewTag, reactViewName = 'RCTView' }: ViewInfo,
@@ -40,15 +51,23 @@ export default class CSSManager implements ICSSManager {
     const [animationProperties, transitionProperties, filteredStyle] =
       filterCSSAndStyleProperties(style);
 
-    const hasAnimationOrTransition =
-      animationProperties !== null || transitionProperties !== null;
-    const normalizedStyle = hasAnimationOrTransition
-      ? this.propsBuilder.build(filteredStyle)
-      : undefined;
+    const hasAnimation = animationProperties !== null;
+    const hasTransition = transitionProperties !== null;
 
-    // We need to update view style only for animations (e.g. when a property is
-    // specified only in a subset of keyframes and start/end value comes from style).
-    if (normalizedStyle && animationProperties) {
+    const normalizedStyle =
+      hasAnimation ||
+      hasTransition ||
+      (IS_ANDROID && this.hadTransitionLastUpdate)
+        ? this.propsBuilder.build(filteredStyle)
+        : undefined;
+
+    if (
+      normalizedStyle &&
+      (hasAnimation ||
+        // We also need to update the current style on Android when the
+        // transition is detached.
+        (IS_ANDROID && !hasTransition && this.hadTransitionLastUpdate))
+    ) {
       setViewStyle(this.viewTag, normalizedStyle);
     }
 
@@ -57,6 +76,8 @@ export default class CSSManager implements ICSSManager {
       normalizedStyle ?? {}
     );
     this.cssAnimationsManager.update(animationProperties);
+
+    this.hadTransitionLastUpdate = hasTransition;
   }
 
   unmountCleanup(): void {

packages/react-native-reanimated/src/css/utils/props.ts
@@ -27,6 +27,13 @@ export function filterCSSAndStyleProperties<S extends AnyRecord>(
   const filteredStyle: AnyRecord = {};
 
   for (const [prop, value] of Object.entries(style)) {
+    if (value === undefined) {
+      // If the user explicitly sets a property to undefined (e.g. when they want
+      // to remove CSS transition or animation), we treat the property as if it was not
+      // present in the style object.
+      continue;
+    }
+
     if (isAnimationProp(prop)) {
       // TODO - add support for animation shorthand
       animationProperties[prop] = value;